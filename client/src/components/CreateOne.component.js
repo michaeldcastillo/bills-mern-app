d3097a3e80b332525c86f016f1e1073a74026cff
Mike Castillo Git <michaeldcastillo@hotmail.com>
2019-05-22 14:49:30 -0500

    save CreateOne working minus API call

--- client/src/components/CreateOne.component.js
+++ client/src/components/CreateOne.component.js
@@ -3,13 +3,15 @@ import axios from 'axios';
 
 class CreateOne extends Component {
 
+    /* --------------------- BIND -------------------- */
     onChangeBillName = this.onChangeBillName.bind(this);
-    onChangeBillPaymentURL = this.onChangeBillPaymentURLe.bind(this);
+    onChangeBillPaymentURL = this.onChangeBillPaymentURL.bind(this);
     onChangeBillDueDate = this.onChangeBillDueDate.bind(this);
     onChangeBillDueAmount = this.onChangeBillDueAmount.bind(this);
     onChangeBillNotes = this.onChangeBillNotes.bind(this);
     onSubmit = this.onSubmit.bind(this);
 
+    /* --------------------- FORM EVENT LISTENERS -------------------- */
     onChangeBillName(event) {
         this.setState({bill_name: event.target.value});
     }
@@ -30,6 +32,7 @@ class CreateOne extends Component {
         this.setState({bill_notes: event.target.value});
     }
 
+    /* --------------------- FORM SUBMIT LISTENER -------------------- */
     onSubmit(event) {
         event.preventDefault(); //prevent default page reload behavior on form submit
 
@@ -43,8 +46,22 @@ class CreateOne extends Component {
            // bill_paid_status: false
         }
         console.log("newBill = ", newBill);
+
+        //http://localhost:5000/api/create
+        //axios.post().then();
+
+        this.setState = ({
+            bill_name: "",
+            bill_payment_url: "",
+            bill_due_date: "",
+            bill_due_amount: "",
+            bill_notes: "",
+            bill_paid_amount: null,
+            bill_paid_status: false
+        });
     }
 
+    /* --------------------- STATE -------------------- */
     //component's initial state
     state = {
         bill_name: "",
@@ -56,10 +73,47 @@ class CreateOne extends Component {
         bill_paid_status: false
     }
 
+    /* --------------------- RENDER -------------------- */
     render() {
         return (
-            <div>
-                <p>CreateOne Component</p>
+            <div style={{border:"2px dotted brown"}}>
+                <p style={{color:"brown"}}><b>CreateOne</b> Component... <b>path="/create"</b></p>
+
+                <div>
+                    <form onSubmit={this.onSubmit}>
+                        {/* bill_name */}
+                        <div className="">
+                            <label><b>bill_name</b> (value = this.state.bill_name):</label><br />
+                            <input type="text" value={this.state.bill_name} onChange={this.onChangeBillName} />
+                        </div>
+                        {/* bill_payment_url */}
+                        <div className="">
+                            <label><b>bill_payment_url</b> (value = this.state.bill_payment_url):</label><br />
+                            <input type="text" value={this.state.bill_payment_url} onChange={this.onChangeBillPaymentURL} />
+                        </div>
+                         {/* bill_due_date */}
+                         <div className="">
+                            <label><b>bill_due_date</b> (value = this.state.bill_due_date):</label><br />
+                            <input type="text" value={this.state.bill_due_date} onChange={this.onChangeBillDueDate} />
+                        </div>
+                        {/* bill_due_amount */}
+                        <div className="">
+                            <label><b>bill_due_amount</b> (value = this.state.bill_due_amount):</label><br />
+                            <input type="text" value={this.state.bill_due_amount} onChange={this.onChangeBillDueAmount} />
+                        </div>
+                            {/* bill_notes */}
+                            <div className="">
+                            <label><b>bill_notes</b> (value = this.state.bill_notes):</label><br />
+                            <input type="text" value={this.state.bill_notes} onChange={this.onChangeBillNotes} />
+                        </div>
+                        {/* submit button */}
+                        <div className="">
+                            <input type="submit" value="Create Bill" className="" />
+                        </div>
+                    </form>
+                </div>
+
+
             </div>
         );
     }
