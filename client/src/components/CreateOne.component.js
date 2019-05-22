315c4bd6075299e7e2d603c2c0844c8ed5135398
Mike Castillo Git <michaeldcastillo@hotmail.com>
2019-05-22 14:04:33 -0500

    save CreateOne

--- client/src/components/CreateOne.component.js
+++ client/src/components/CreateOne.component.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-//import axios from 'axios';
+import axios from 'axios';
 
 class CreateOne extends Component {
 
@@ -10,17 +10,40 @@ class CreateOne extends Component {
     onChangeBillNotes = this.onChangeBillNotes.bind(this);
     onSubmit = this.onSubmit.bind(this);
 
-    onChangeBillName(event) {}
+    onChangeBillName(event) {
+        this.setState({bill_name: event.target.value});
+    }
 
-    onChangeBillPaymentURL(event) {}
+    onChangeBillPaymentURL(event) {
+        this.setState({bill_payment_url: event.target.value});
+    }
 
-    onChangeBillDueDate(event) {}
+    onChangeBillDueDate(event) {
+        this.setState({bill_due_date: event.target.value});
+    }
 
-    onChangeBillDueAmount(event) {}
+    onChangeBillDueAmount(event) {
+        this.setState({bill_due_amount: event.target.value});
+    }
 
-    onChangeBillNotes(event) {}
+    onChangeBillNotes(event) {
+        this.setState({bill_notes: event.target.value});
+    }
 
-    onSubmit(event) {}
+    onSubmit(event) {
+        event.preventDefault(); //prevent default page reload behavior on form submit
+
+        const newBill = {
+            bill_name: this.state.bill_name,
+            bill_payment_url: this.state.bill_payment_url,
+            bill_due_date: this.state.bill_due_date,
+            bill_due_amount: this.state.bill_due_amount,
+            bill_notes: this.state.bill_notes
+           // bill_paid_amount: null,
+           // bill_paid_status: false
+        }
+        console.log("newBill = ", newBill);
+    }
 
     //component's initial state
     state = {
