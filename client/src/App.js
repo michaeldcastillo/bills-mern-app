e36dbd2498d3c5fbde9f5a8738d51999cfd74c26
Mike Castillo Git <michaeldcastillo@hotmail.com>
2019-05-22 12:04:58 -0500

    CreateOne save

--- client/src/App.js
+++ client/src/App.js
@@ -5,18 +5,25 @@ import ReadAll from "./components/ReadAll.component.js";
 import UpdateOne from "./components/UpdateOne.component.js";
 //import "./App.css";
 //import "bootstrap/dist/css/bootstrap.min.css";
+import logo from "./logo.svg";
 
 class App extends Component {
   render() {
     return (
       <Router>
+
         <div>
-          <h2>App Component</h2>
+          <img src={logo} width="" height="30px" alt="" /><h2 style={{display:"inline-block"}}>App Component</h2>
+          <br />
+          <Link to="/">ReadAll</Link> | <Link to="/create">CreateOne</Link>
         </div>
+
         <br />
+        
         <Route path="/create" component={CreateOne} />
         <Route exact path="/" component={ReadAll} /> 
         <Route path="/update/:id" component={UpdateOne} />
+
       </Router>
     );
   }
--- client/src/components/CreateOne.component.js
+++ client/src/components/CreateOne.component.js
@@ -1,6 +1,38 @@
 import React, { Component } from "react";
+//import axios from 'axios';
 
 class CreateOne extends Component {
+
+    onChangeBillName = this.onChangeBillName.bind(this);
+    onChangeBillPaymentURL = this.onChangeBillPaymentURLe.bind(this);
+    onChangeBillDueDate = this.onChangeBillDueDate.bind(this);
+    onChangeBillDueAmount = this.onChangeBillDueAmount.bind(this);
+    onChangeBillNotes = this.onChangeBillNotes.bind(this);
+    onSubmit = this.onSubmit.bind(this);
+
+    onChangeBillName(event) {}
+
+    onChangeBillPaymentURL(event) {}
+
+    onChangeBillDueDate(event) {}
+
+    onChangeBillDueAmount(event) {}
+
+    onChangeBillNotes(event) {}
+
+    onSubmit(event) {}
+
+    //component's initial state
+    state = {
+        bill_name: "",
+        bill_payment_url: "",
+        bill_due_date: "",
+        bill_due_amount: "",
+        bill_notes: "",
+        bill_paid_amount: null,
+        bill_paid_status: false
+    }
+
     render() {
         return (
             <div>
